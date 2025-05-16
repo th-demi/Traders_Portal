fff81321257940757aefe4bbcd613347e2dfdcbe
Demi ✨ <demi@Demis-MacBook-Pro.local>
2025-05-16 16:19:48 +0530

    Read ALLOWED_HOSTS from environment variable

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = 'django-insecure-_i&*pa_&f(*!c0u6mrf@6p4h=o-=ut40d+s!qu+!5k#garbct=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
